2ac156f9a34128de0ce452c3a25da2fc281b41ab
ainoka <ainaabdyldaeva@gmail.com>
2020-10-28 18:08:26 -0500

    add dialog.sh

---
+++ dialog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello!"
+sleep 2
+echo "What is your name?" #Enter your name 
+sleep 2
+read NAME
+sleep 2
+echo "Hello $NAME!"
+sleep 2
+echo "How is your day?" #Answer for question
+read answer
+echo "Good to hear, you are doing $answer."
+
